03308f3e3c670420106154ec6db7e94b4322acec
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-19 14:48:13 -0500

    Update Zoologico.py

--- gestion/Zoologico.py
+++ gestion/Zoologico.py
@@ -11,8 +11,29 @@ class Zoologico():
         total=0
         for Zona in self._zonas:
             total += Zona.cantidadAnimales
+        return total
 
     def agregarZonas(self, zona):
         self._zonas.append(zona)
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion
+    
+    def getUbicacion(self):
+        return self._ubicacion
+    
+    @classmethod
+    def setZonas(cls, zonas):
+        cls._zonas = zonas
+    
+    @classmethod
+    def getZonas(cls):
+        return cls._zonas
 
             
