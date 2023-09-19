405a6cebca84ff5f64391d152f413c3f4c33255a
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-19 16:50:30 -0500

    Update anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -9,7 +9,7 @@ class Anfibio(Animal):
     def __init__(self, nombre, edad, habitat, genero, colorPiel, venenoso):
         super().__init__(nombre, edad, habitat, genero)
         self._colorPiel =colorPiel
-        self.venenoso = venenoso
+        self._venenoso = venenoso
 
     @staticmethod
     def cantidadAnfibios():
