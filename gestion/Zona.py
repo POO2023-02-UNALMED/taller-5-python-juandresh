f68a7efa8c25ac8ce65ebd76c817159455fa266a
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-19 14:48:26 -0500

    Update Zona.py

--- gestion/Zona.py
+++ gestion/Zona.py
@@ -11,3 +11,23 @@ class Zona():
 
     def cantidadAnimales(self):
         return (len(self._animales))
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+
+    def setZoo(self, zoo):
+        self._zoo = zoo
+    
+    def getZoo(self):
+        return self._zoo
+    
+    @classmethod
+    def setAnimales(cls, animales):
+        cls._animales = animales
+    
+    @classmethod
+    def getAnimales(cls):
+        return cls._animales
