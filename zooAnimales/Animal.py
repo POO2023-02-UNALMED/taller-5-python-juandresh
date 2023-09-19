dba5071306c0da8c31b4372ecbba2f8415f2ae5b
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-19 14:48:44 -0500

    Update Animal.py

--- zooAnimales/Animal.py
+++ zooAnimales/Animal.py
@@ -20,5 +20,44 @@ class Animal():
         if self._zona == None:
             return(f'Mi nombre es {self._nombre}, dengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}') 
         else:   
-            return(f'Mi nombre es {self._nombre}, dengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona}')
+            return(f'Mi nombre es {self._nombre}, dengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona.getZona()}, en el {self._zona.getZona().getZoo()}')
     
+    @classmethod
+    def setTotalAnimales(cls, totalAnimales):
+        cls._totalAnimales = totalAnimales
+    
+    @classmethod
+    def getTotalAnimales(cls):
+        return cls._totalAnimales
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+    
+    def setEdad(self, edad):
+        self._edad = edad
+    
+    def getEdad(self):
+        return self._edad
+    
+    def setHabitat(self, habitat):
+        self._habitat = habitat
+    
+    def getHabitat(self):
+        return self._habitat
+    
+    def setGenero(self, genero):
+        self._genero = genero
+    
+    def getGenero(self):
+        return self._genero
+    
+    @classmethod
+    def setZona(cls, zona):
+        cls._zona = zona
+    
+    @classmethod
+    def getZona(cls):
+        return cls._zona
